48c6088823a3e9016b72275c722bca4bdf75f7bf
Anthony White <awhite@dataxu.com>
2013-02-02 20:46:32 -0500

    Add button to add views

--- Boxey/app/app_delegate.rb
+++ Boxey/app/app_delegate.rb
@@ -7,6 +7,24 @@ class AppDelegate
     @blue_view.backgroundColor = UIColor.blueColor
     @window.addSubview @blue_view
 
+    @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    @button.setTitle('Add', forState: UIControlStateNormal)
+    @button.sizeToFit
+    @button.frame = CGRect.new [10, @window.frame.size.height - 10 - @button.frame.size.height], @button.frame.size
+    @window.addSubview @button
+
+    @button.addTarget(self, action: 'add_tapped', forControlEvents: UIControlEventTouchUpInside)
+
     true
+  end
+
+  def add_tapped
+    new_view = UIView.alloc.initWithFrame(CGRect.new([0,0], [100,100]))
+    new_view.backgroundColor = UIColor.blueColor
+    last_view = @window.subviews.first
+
+    new_view.frame = CGRect.new([last_view.frame.origin.x, last_view.frame.origin.y + last_view.frame.size.height + 10], last_view.frame.size)
+
+    @window.insertSubview(new_view, atIndex: 0)
   end
 end
