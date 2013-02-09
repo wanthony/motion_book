a7f528bf3be53e2bbf12ce35375ef8f6f5ebea7a
Anthony White <awhite@dataxu.com>
2013-02-09 11:59:06 -0500

    Allow user to change box color

--- Boxey/app/app_delegate.rb
+++ Boxey/app/app_delegate.rb
@@ -3,9 +3,13 @@ class AppDelegate
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.applicationFrame)
     @window.makeKeyAndVisible
 
+    @box_color = UIColor.blueColor
+
     @blue_view = UIView.alloc.initWithFrame(CGRect.new([10,10], [100, 100]))
-    @blue_view.backgroundColor = UIColor.blueColor
+    @blue_view.backgroundColor = @box_color
     @window.addSubview @blue_view
+
+    add_labels_to_boxes
 
     @button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
     @button.setTitle('Add', forState: UIControlStateNormal)
@@ -23,21 +27,51 @@ class AppDelegate
 
     @remove_button.addTarget(self, action: 'remove_tapped', forControlEvents: UIControlEventTouchUpInside)
 
+    @color_field = UITextField.alloc.initWithFrame(CGRectZero)
+    @color_field.borderStyle = UITextBorderStyleRoundedRect
+    @color_field.text = "Blue"
+    @color_field.enablesReturnKeyAutomatically = true
+    @color_field.returnKeyType = UIReturnKeyDone
+    @color_field.autocapitalizationType = UITextAutocapitalizationTypeNone
+    @color_field.sizeToFit
+    @color_field.frame = CGRect.new([@blue_view.frame.origin.x + @blue_view.frame.size.width + 10, @blue_view.frame.origin.y + @color_field.frame.size.height], @color_field.frame.size)
+    @window.addSubview @color_field
+
+    @color_field.delegate = self
+
     true
+  end
+
+  def textFieldShouldReturn textField
+    color_tapped
+    textField.resignFirstResponder
+    false
+  end
+
+  def color_tapped
+    color_prefix = @color_field.text
+    color_method = "#{color_prefix.downcase}Color"
+    if UIColor.respond_to?(color_method)
+      @box_color = UIColor.send(color_method)
+      boxes.each {|box| box.backgroundColor = @box_color }
+    else
+      UIAlertView.alloc.initWithTitle("Invalid Color", message: "#{color_prefix} is not a valid color", delegate: nil, cancelButtonTitle: "OK", otherButtonTitles: nil).show
+    end
   end
 
   def add_tapped
     new_view = UIView.alloc.initWithFrame(CGRect.new([0,0], [100,100]))
-    new_view.backgroundColor = UIColor.blueColor
+    new_view.backgroundColor = @box_color
     last_view = @window.subviews.first
 
     new_view.frame = CGRect.new([last_view.frame.origin.x, last_view.frame.origin.y + last_view.frame.size.height + 10], last_view.frame.size)
 
     @window.insertSubview(new_view, atIndex: 0)
+    add_labels_to_boxes
   end
 
   def remove_tapped
-    other_views = @window.subviews.reject {|view| view.is_a? UIButton }
+    other_views = boxes
     @last_view = other_views.last
 
     if @last_view && other_views.count > 1
@@ -51,8 +85,33 @@ class AppDelegate
                                    end
                                  },
 
-                                 completion: lambda {|finished| @last_view.removeFromSuperview }
+                                 completion: lambda {|finished|
+                                   @last_view.removeFromSuperview
+                                   add_labels_to_boxes
+                                 }
                                  )
     end
+  end
+
+  def boxes
+    @window.subviews.reject {|view| view.is_a?(UIButton) || view.is_a?(UILabel) || view.is_a?(UITextField) }
+  end
+
+  def add_labels_to_boxes
+    boxes.each {|box| add_label_to_box(box) }
+  end
+
+  def add_label_to_box box
+    box.subviews.each(&:removeFromSuperview)
+
+    index_of_box = @window.subviews.index(box)
+    label = UILabel.alloc.initWithFrame(CGRectZero)
+    label.text = index_of_box.to_s
+    label.textColor = UIColor.whiteColor
+    label.backgroundColor = UIColor.clearColor
+    label.sizeToFit
+    label.center = [box.frame.size.width / 2, box.frame.size.height / 2]
+
+    box.addSubview(label)
   end
 end
